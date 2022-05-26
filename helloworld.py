2f95401ea0533eb0eeba35a0d84cc5958a3d33d1
Sean Council Choate <seannchoate@gmail.com>
2022-05-26 10:05:40 -0500

    updated helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-def hello_world(state):
-    print(f'Hello World {state}!')
+def hello_world(city, state):
+    print(f'Hello World {city}, {state}!')
 
 
 def main():
-    hello_world('New York')
-=======
-def hello_world(city):
-    print(f'Hello World {city}!')
-
-
-def main():
-    hello_world('NYC')
->>>>>>> e7e10ca620b15939026103dc97e27df54783328f
+    hello_world('NYC', 'New York')
 
 
 main()
